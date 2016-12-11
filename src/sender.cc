aaa730ac07d65fcab86002dae576dd325d3bb643
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-12-11 22:53:31 +0100

    re-enable logging

--- src/sender.cc
+++ src/sender.cc
@@ -3,7 +3,7 @@
 //
 
 #include <broker/broker.hh>
-//#include <acu/logger.h>
+#include <acu/logger.h>
 #include "acu/sender.h"
 #include "broker/endpoint.hh"
 
@@ -12,20 +12,20 @@ namespace acu {
     using namespace broker;
 
     bool Sender::Send(OutgoingAlert *alert) {
-        //auto log = Logger::GetLogger("Sender");
+        auto log = Logger::GetLogger("Sender");
         endpoint ep(ENDPOINT_NAME);
 
         // default retry interval is 5 seconds. Maybe we want to change that?
         peering success = ep.peer(this->destination, this->port);
         if ( !success | (ep.outgoing_connection_status().want_pop().front().status !=
              outgoing_connection_status::tag::established)) {
-            //log->Warn("Sender: Error peering with", this->destination, this->port);
+            log->Warn("Sender: Error peering with", this->destination, this->port);
             return false;
         }
 
         // TODO: do we want to use the incidentName as topic?
         ep.send(alert->incidentName, alert->ToMessage());
-        //log->Debug("Sender sent on topic", alert->incidentName);
+        log->Debug("Sender sent on topic", alert->incidentName);
         return true;
     }
 }
