74099f519d5151859013e0162014d37246a53119
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-12-13 17:02:11 +0100

    remove logger again, add comment
    logger still seems to misbehave "sometimes".
    removed all usages of logger, to be safe here.

--- src/sender.cc
+++ src/sender.cc
@@ -3,7 +3,6 @@
 //
 
 #include <broker/broker.hh>
-#include <acu/logger.h>
 #include "acu/sender.h"
 #include "broker/endpoint.hh"
 
@@ -14,20 +13,20 @@ namespace acu {
     using namespace broker;
 
     bool Sender::Send(OutgoingAlert *alert) {
-        auto log = Logger::GetLogger("Sender");
+
+        // TBD ob ein einmaliges peering im konstruktor sinnvoller wäre.
+        // das hier ist erstmal die "safe" variante, mit der wir starten können.
         endpoint ep(ENDPOINT_NAME);
 
         // default retry interval is 5 seconds. Maybe we want to change that?
         peering success = ep.peer(this->destination, this->port);
         if (!success || ep.outgoing_connection_status().want_pop().front().status
                         != outgoing_connection_status::tag::established) {
-            log->Warn("Sender: Error peering with ", this->destination, this->port);
             return false;
         }
 
         // TODO: do we want to use the incidentName as topic?
         ep.send(alert->incidentName, alert->ToMessage());
-        log->Debug("Sender sent on topic", alert->incidentName);
         return true;
     }
 }
