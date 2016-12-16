1e04f238faa1414a56fdc31487cde0049dd9bb2b
Dave J <chronus@protonmail.com>
2016-12-16 17:19:46 +0100

    basic setup/rework of acu

--- include/acu/acu.h
+++ include/acu/acu.h
@@ -1,22 +1,35 @@
-//
-// Created by florian on 11/30/16.
-//
-
 #ifndef ACU_FW_ACU_H
 #define ACU_FW_ACU_H
 
+#include "aggregation.h"
+#include "correlation.h"
 #include "receiver.h"
 #include "sender.h"
+
+#include <unordered_map>
 
 namespace acu {
 
     class Acu {
     public:
-        Acu();
+        Acu() : aggregations(new std::unordered_map<std::string, Aggregation*>()),
+                correlations(new std::unordered_map<std::string, Correlation*>()) {};
+
+        /// Register aggregations and correlations on the given topics.
+        ///
+        /// **aggregation** may be NULL.
+        ///
+        /// @param topics       The topics to register to.
+        /// @param aggregation  The Aggregation to register on those topics.
+        /// @param correlation  The Correlation to register on those topics.
+        void Register(std::vector<std::string> *topics, Aggregation *aggregation, Correlation *correlation);
 
     private:
-        Receiver receiver;
-        Sender sender;
+        Receiver *receiver;
+        Sender *sender;
+
+        std::unordered_map<std::string, Aggregation*> *aggregations;
+        std::unordered_map<std::string, Correlation*> *correlations;
     };
 }
 
--- src/acu.cc
+++ src/acu.cc
@@ -1,5 +1,18 @@
-//
-// Created by florian on 11/30/16.
-//
-
 #include "acu/acu.h"
+
+namespace acu {
+
+    void Acu::Register(std::vector<std::string> *topics, Aggregation *aggregation, Correlation *correlation) {
+        // asserts
+        assert(topics != nullptr);
+        assert(correlation != nullptr);
+
+        // iterate of topics to add maps
+        for (auto topic : topics) {
+            if (aggregations.count(topic)) {
+                aggregations.emplace(topic, aggregation);
+            }
+            correlations.emplace(topic, correlation);
+        }
+    }
+ }
