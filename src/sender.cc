cb8c350f3c836bdb28ed7d2ab80112d0749df065
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-12-17 18:35:43 +0100

    rewrite sender
    sender initially peers with the remote endpoint in the constructor,
    retrying every 5 secs if this does not succeed.
    
    On every send, the connection is re-validated.

--- src/sender.cc
+++ src/sender.cc
@@ -1,49 +1,32 @@
-//
-// Sends data via broker to given destination + port.
-//
+/*
+ * Sends data via broker to given destination + port.
+ */
 
 #include <broker/broker.hh>
 #include <iostream>
-#include <unistd.h>
 #include "acu/sender.h"
-#include "broker/endpoint.hh"
-
 
 namespace acu {
     // This could also be set via config file?
     std::string const ENDPOINT_NAME = "ACU Sender";
 
-    using namespace broker;
+    Sender::Sender(std::string destination, port_t port) :
+            endpoint(new broker::endpoint(ENDPOINT_NAME)) {
+
+        // default retry interval is 5 seconds. Maybe we want to change that?
+        endpoint->peer(destination, port);
+    }
 
     bool Sender::Send(OutgoingAlert *alert) {
 
-        // TBD ob ein einmaliges peering im konstruktor sinnvoller wäre.
-        // das hier ist erstmal die "safe" variante, mit der wir starten können.
-        endpoint ep(ENDPOINT_NAME);
-
-        // default retry interval is 5 seconds. Maybe we want to change that?
-        ep.peer(destination, port);
-
-        // ep.outgoing_connection_status().want_pop().front() returns a non-null,
-        // default constructed outgoing_connection_status.
-        // the default result of outgoing_connection_status.status is undefined.
-        // since "status" is modeled as enum, undefined is not an option and the default enum is resolved.
-        // The default enum is the first value enumerated in the enum.
-        // That is "established".
-        // So any outgoing_connection_status is always "established", when it is initialized and later on
-        // it may be set to "disconnected". Why it does not start with "disconnected" is a riddle to me.
-        usleep(100 * 1000);
-
-        if (ep.outgoing_connection_status().want_pop().front().status
-                != outgoing_connection_status::tag::established) {
+        auto conn_stati = endpoint->outgoing_connection_status().want_pop();
+        if (conn_stati.size() == 0
+            || conn_stati.front().status != broker::outgoing_connection_status::tag::established) {
 
             return false;
         }
-        std::cout << "sender peer success with " + destination + ":";
-        std::cout << port;
-        std::cout << std::endl;
         // TODO: do we want to use the incidentName as topic?
-        ep.send(alert->incidentName, alert->ToMessage());
+        endpoint->send(alert->incidentName, alert->ToMessage());
         return true;
     }
 }
--- test/test_sender.cc
+++ test/test_sender.cc
@@ -1,14 +1,9 @@
-//
-// Created by flx on 11.12.16.
-//
-
 #include <catch.hpp>
 #include <acu/sender.h>
-#include <broker/endpoint.hh>
 #include <broker/message_queue.hh>
-#include <broker/broker.hh>
 #include <iostream>
 #include <unistd.h>
+#include <broker/broker.hh>
 
 // Provides a mock implementation for OutgoingAlert
 class MockOutgoingAlert : public acu::OutgoingAlert {
@@ -39,11 +34,7 @@ TEST_CASE("Testing sender class layout", "[sender]") {
 
 TEST_CASE("Testing sender send functionality", "[sender]") {
     // setup
-    std::string local_ip = "127.0.0.1";
-    acu::port_t local_port = 9999;
-
-    acu::Sender *sender = new acu::Sender(local_ip, local_port);
-    REQUIRE(sender != nullptr);
+    broker::init();
 
     auto alertName = "MyAlert";
     auto alertTime = std::chrono::system_clock::now();
@@ -51,11 +42,19 @@ TEST_CASE("Testing sender send functionality", "[sender]") {
     REQUIRE_FALSE(mockAlert->ToMessageCalled());
 
     SECTION("Testing successful send") {
+        std::string local_ip = "127.0.0.1";
+        acu::port_t local_port = 9999;
+
         // remote bro-broker "mock" via localhost
-        broker::init();
         broker::endpoint rec_ep("Receiver Endpoint");
         rec_ep.listen(local_port, local_ip.c_str());
-        usleep(100 * 1000); // listen may take a while
+        usleep(300 * 1000); // wait for listen to grip...
+
+        acu::Sender *sender = new acu::Sender(local_ip, local_port);
+        REQUIRE(sender != nullptr);
+
+        // wait on this site with the mocked receiver:
+        rec_ep.incoming_connection_status().need_pop();
 
         broker::message_queue queue(alertName, rec_ep);
 
@@ -74,7 +73,8 @@ TEST_CASE("Testing sender send functionality", "[sender]") {
     SECTION("Testing sending without peering") {
 
         // provide invalid address...
-        sender = new acu::Sender(local_ip, 1234);
+        acu::Sender *sender = new acu::Sender("127.0.0.1", 1234);
+
         // do test
         bool success = sender->Send(mockAlert);
 
