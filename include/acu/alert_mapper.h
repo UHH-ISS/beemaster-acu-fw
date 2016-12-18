1f0e9d73773d86bc0fb4cb798e0ff1e64dfb5892
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2016-12-18 23:17:00 +0100

    add raw skeleton for alert mapper
    as discussed in mumble, we want to encapsulate the functionality of
    mapping some broker message to a specific incoming alert type inside an
    extra mapper. this adds a mapper headerfile without tests.

---
+++ include/acu/alert_mapper.h
@@ -0,0 +1,21 @@
+//
+// Created by flx on 18.12.16.
+//
+
+#ifndef ACU_FW_ALERT_MAPPER_H
+#define ACU_FW_ALERT_MAPPER_H
+
+#include "incoming_alert.h"
+
+namespace acu {
+
+    class AlertMapper {
+
+    public:
+        AlertMapper() : {};
+
+        IncomingAlert GetAlert(std::string topic, broker::message &msg);
+
+    };
+}
+#endif //ACU_FW_ALERT_MAPPER_H
